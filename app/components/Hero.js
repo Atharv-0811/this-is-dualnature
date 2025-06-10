fbf96b3e72dbf430f037af78968593eced8b0e29
AtharvC0811 <1032222805@mitwpu.edu.in>
2025-06-11 00:51:37 +0530

    rose lenses fix 2

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -732,7 +732,7 @@ export default function Hero() {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ delay: 2, duration: 1 }}
-        className="absolute bottom-8 left-1/2 transform -translate-x-1/2"
+        className="absolute bottom-8 left-1/2 transform -translate-x-1/2 hidden md:block"
       >
         <motion.div
           animate={{ y: [0, 10, 0] }}
